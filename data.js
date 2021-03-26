39fa7bc1484d6e93f5eac1cb2b4c0216f1c94829
Igor Ináš <igor.inas@toptal.com>
2021-03-26 08:53:03 +0100

    Smart collections

--- data.js
+++ data.js
@@ -1,15 +1,15 @@
-const COUNTRIES = [
-    {id: "00001", name: "USA", planet: {id: "00003"}},
-    {id: "00002", name: "Russia", planet: {id: "00003"}},
-    {id: "00003", name: "China", planet: {id: "00003"}},
-    {id: "00004", name: "Musktopia", planet: {id: "00004"}},
-];
-
 const PLANETS = [
     {id: "00001", name: "Mercury"},
     {id: "00002", name: "Venus"},
     {id: "00003", name: "Earth"},
     {id: "00004", name: "Mars"},
+];
+
+const COUNTRIES = [
+    {id: "00001", name: "USA", planet: PLANETS.find(planet => planet.id === "00003")},
+    {id: "00002", name: "Russia", planet: PLANETS.find(planet => planet.id === "00003")},
+    {id: "00003", name: "China", planet: PLANETS.find(planet => planet.id === "00003")},
+    {id: "00004", name: "Musktopia", planet: PLANETS.find(planet => planet.id === "00004")},
 ];
 
 module.exports = {COUNTRIES, PLANETS}
